d4df144ea3e4e20074c517ae9057bb50a5b9ee3d
Ry Sutton <ryancesutton@gmail.com>
2020-05-19 14:21:54 -0500

    make some adjustments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,11 +9,13 @@ public class Main {
         System.out.println("Enter Something: ");
         String userInput = scanner.nextLine();
         System.out.println("You Entered: " + userInput);
-        System.out.println(userInput + " is not a number");
-        System.out.println(StringUtils.swapCase(userInput));
-        System.out.println(StringUtils.reverse(userInput));
-
-
+        if (!StringUtils.isNumeric(userInput)) {
+            System.out.println(userInput + " is not a number");
+            System.out.println(StringUtils.swapCase(userInput));
+            System.out.println(StringUtils.reverse(userInput));
+        } else {
+            System.out.println("Invalid input");
+        }
 
     }
 
