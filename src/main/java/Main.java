0fda6f2a0e3561db022c9771fc053f7098159b99
Ry Sutton <ryancesutton@gmail.com>
2020-05-19 11:47:23 -0500

    add apache commons lang and set up main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Something: ");
+        String userInput = scanner.nextLine();
+        System.out.println("You Entered: " + userInput);
+
+    }
+
+}
